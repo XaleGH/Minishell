8320272afc06100fb605c47b741a200ced9e5b7d
XaleGH <axel.saux@gmail.com>
2024-05-29 18:12:58 +0200

    Structure de base

---
+++ inc/minishell.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaux <asaux@student.42perpignan.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 18:03:50 by asaux             #+#    #+#             */
+/*   Updated: 2024/05/29 18:06:37 by asaux            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+//Librairy
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+//Struct
+
+
+
+/* Fonctions */
+
+//mainc.c
+int	main();
+
+//
+
+//
+
+//
+
+//
+
+//
+
+//
+
+#endif
