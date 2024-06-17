931f8b5b0dbf1f2edbc80928522e916bbb1197ca
jawamad <jawamad@gmail.com>
2024-06-17 17:11:27 +0200

    dup env and shlvl increment

--- src/main.c
+++ src/main.c
@@ -3,12 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asaux <asaux@student.42perpignan.fr>       +#+  +:+       +#+        */
+/*   By: flmuller <flmuller@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 18:03:17 by asaux             #+#    #+#             */
-/*   Updated: 2024/05/29 18:03:30 by asaux            ###   ########.fr       */
+/*   Updated: 2024/06/17 16:09:21 by flmuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+int	main(int ac, char **av, char **env)
+{
+	t_data	data;
+
+	(void) av;
+	if (ac > 1)
+		return (printf("Error, no argument needed\n"),1);
+	data.env = dupenv(env);
+	addshlvl(&data);
+	free_array(data.env);
+}
